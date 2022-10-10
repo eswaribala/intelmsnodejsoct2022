667fe7d57105e7557e9446721e94dd4bb0e0efda
eswaribala <parameswaribala@gmail.com>
2022-10-11 00:16:49 +0530

    customer api updated

--- customerapi/server.js
+++ customerapi/server.js
@@ -1,11 +1,16 @@
 const express=require("express");
+var bodyParser=require('body-parser');
 const app=express();
 //rest methods get,post,put,delete,patch
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.get('/',function(req,res){
     res.send('Customer API Ready to jfvdsdskhfgsdf Rock');
 })
 
+app.post('/customers',function (req,res){
+    console.log(req.body);
+})
 
 app.listen(3000,function(){
     console.log("Listening on Port 3000")
