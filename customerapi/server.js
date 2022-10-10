996a2491bbb6314f2d7ba23fe48926298b042cfe
eswaribala <parameswaribala@gmail.com>
2022-10-10 23:53:01 +0530

    customer api updated

--- customerapi/server.js
+++ customerapi/server.js
@@ -1,5 +1,12 @@
 const express=require("express");
 const app=express();
+//rest methods get,post,put,delete,patch
+
+app.get('/',function(req,res){
+    res.send('Customer API Ready');
+})
+
+
 app.listen(3000,function(){
     console.log("Listening on Port 3000")
 })
