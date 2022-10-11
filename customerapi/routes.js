aa119aacd4e14eb06df618ac9a2c5afead4033d9
eswaribala <parameswaribala@gmail.com>
2022-10-11 23:21:35 +0530

    updated config

--- customerapi/routes.js
+++ customerapi/routes.js
@@ -3,5 +3,8 @@ module.exports = app => {
     var router = require("express").Router();
     // Create a new Customer
     router.post("/", controller.create);
+    //Retrieve customers data
+    router.get("/",controller.findAllCustomers);
+
     app.use("/api/customers", router);
 }
